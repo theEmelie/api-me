1dd70aef1dc0b5bdc0326f14d337142db1263b31
unknown <theem@live.se>
2019-09-20 21:04:37 +0100

    updated routes

--- routes/auth.js
+++ routes/auth.js
@@ -3,6 +3,7 @@ const router = express.Router();
 
 const auth = require("../models/auth.js");
 
+console.log(req.body);
 router.post('/login', (req, res) => auth.login(res, req.body));
 router.post('/register', (req, res) => auth.register(res, req.body));
 
